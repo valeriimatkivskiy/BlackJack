7c4ad1cbc90292b4456200cc6125cb52c7c254ae
Valerii Matkivskiy <vmatkivskii@gmail.com>
2015-08-15 12:12:47 +0300

    Add method dealNextCard

--- src/com/blackjack/Deck.java
+++ src/com/blackjack/Deck.java
@@ -1,5 +1,7 @@
 package com.blackjack;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -25,29 +27,41 @@ public class Deck {
             System.exit(1);
         }
     }
-    public void swapCards(Card firstCard, Card secondCard) {
-        Card temp = firstCard;
-        firstCard = secondCard;
-        secondCard = temp;
-    }
+
     public void shuffleDeck() {
         Random randomGenerator = new Random();
         for (int cardIndex = 0; cardIndex < cardList.size(); cardIndex++) {
             int cardToSwap = randomGenerator.nextInt(cardList.size());
             System.out.println("Before Swap: " + cardIndex + " " + cardList.get(cardIndex).toString() + " and " + cardToSwap + " " + cardList.get(cardToSwap).toString());
-            swapCards(cardList.get(cardIndex), cardList.get(cardToSwap));
+            Collections.swap(this.cardList, cardIndex, cardToSwap);
             System.out.println("After Swap: " + cardIndex + " " + cardList.get(cardIndex).toString() + " and " + cardToSwap + " " + cardList.get(cardToSwap).toString());
         }
     }
-    public void showDeck() {
-        for (int cardIndex = 0; cardIndex < cardList.size(); cardIndex++) {
-            System.out.println(cardList.get(cardIndex).toString());
+    public void showDeck(int numberOfCards) {
+        if (numberOfCards <= cardList.size()) {
+            for (int cardIndex = 0; cardIndex < numberOfCards; cardIndex++) {
+                System.out.println("Card #" + (cardIndex + 1) + " : " + cardList.get(cardIndex).toString());
+            }
+            if (numberOfCards < cardList.size()) {
+                System.out.println("And " + (cardList.size() - numberOfCards) + " others.");
+            }
+        }
+        else {
+            System.err.println("Error: Number of Card is not valid");
+            System.exit(1);
         }
     }
-
-    public static void main(String[] args) {
-        Deck deck = new Deck(4);
-        deck.shuffleDeck();
-        deck.showDeck();
+    public Card dealNextCard() {
+        Card cardToDeal = null;
+        if (!cardList.isEmpty()) {
+            cardToDeal = cardList.getFirst();
+            cardList.removeFirst();
+        }
+        else {
+            System.err.println("ERROR: Can't deal a card! Deck is empty.");
+            System.exit(1);
+        }
+        return cardToDeal;
     }
+
 }
